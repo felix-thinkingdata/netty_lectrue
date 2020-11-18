61b2f50b421a49b2e3814e9141e5a428816e8b69
felix.Wang <felix@thinkingdata.cn>
2020-11-19 00:09:57 +0800

    Initial commit

---
+++ src/main/java/cn/thinkingdata/netty/firstdemo/TestServer.java
@@ -0,0 +1,31 @@
+package cn.thinkingdata.netty.firstdemo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TestServer {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //事件按循环组
+        EventLoopGroup boosGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(workerGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitialzer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        } finally {
+            boosGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+}
