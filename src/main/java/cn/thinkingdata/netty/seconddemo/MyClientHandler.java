73070832884fe56a4c0ac89ea7418687f6eae7d1
felix.Wang <felix@thinkingdata.cn>
2020-11-19 13:46:33 +0800

    second demo

---
+++ src/main/java/cn/thinkingdata/netty/seconddemo/MyClientHandler.java
@@ -0,0 +1,27 @@
+package cn.thinkingdata.netty.seconddemo;
+
+import java.time.LocalDate;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class MyClientHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(ctx.channel().remoteAddress());
+        System.out.println("clinet output : " + msg);
+        ctx.writeAndFlush("from client: " + LocalDate.now());
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.channel().writeAndFlush("active");
+    }
+}
