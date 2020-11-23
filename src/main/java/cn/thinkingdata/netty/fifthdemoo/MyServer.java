4fff433906fdc37e230194ceff90593900115df6
felix.Wang <felix@thinkingdata.cn>
2020-11-23 10:51:48 +0800

    增加protoc的依赖

---
+++ src/main/java/cn/thinkingdata/netty/fifthdemoo/MyServer.java
@@ -0,0 +1,31 @@
+package cn.thinkingdata.netty.fifthdemoo;
+
+import java.net.InetSocketAddress;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class MyServer {
+    public static void main(String[] args) throws InterruptedException {
+        //事件循环组
+        EventLoopGroup boosGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(workerGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new WebSocketChannelInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(new InetSocketAddress(8899)).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        } finally {
+            boosGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
